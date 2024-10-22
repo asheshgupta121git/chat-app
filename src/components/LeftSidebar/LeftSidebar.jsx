880fafde516846c91346b499352fdba6959565f9
ashesh <guptaashesh180@gmail.com>
2024-10-22 15:49:44 +0530

    add chat-app

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./LeftSidebar.css";
+import assets from "../../assets/assets";
+
+const LeftSidebar = () => {
+  return (
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className="logo" alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+            <div className="sub-menu">
+              <p>Edit Profile</p>
+              <hr />
+              <p>Logout</p>
+            </div>
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder="search .." />
+        </div>
+      </div>
+      <div className="ls-list">
+        {Array(12)
+          .fill("")
+          .map((item, index) => (
+            <div key={index} className="friends">
+              <img src={assets.profile_img} alt="" />
+              <div>
+                <p>ashesh stan</p>
+                <span>Hello hows'there</span>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
